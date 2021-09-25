ba1262f96fc0007c9880e2ce669dbb6f17907e5e
EngineeredReiwa <k.keisuke.1230@gmail.com>
2021-09-25 04:33:05 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,21 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 255 }
+  has_many :favoriteds, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favoritedbys, through: :favorites, source: :user
+  
+  def favorited(user)
+    unless self == user
+      self.favoriteds.find_or_create_by(user_id: user.id)
+    end
+  end
+
+  def unfavorited(user)
+    favorited = self.favoriteds.find_by(user_id: user.id)
+    favorited.destroy if favorited
+  end
+
+  def favoriteding?(user)
+    self.favoritedbys.include?(user)
+  end
 end
